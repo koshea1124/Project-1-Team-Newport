51a62688c9c2ea7ef570b1154230a43c46cbe09b
HMiramontez <miramontez.h@gmail.com>
2023-01-23 21:24:48 -0800

    pseudo code for javascript

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// when button is clicked a total of five workout videos  will appear and be embedded on the page to bypass admin firewalls if necessary and give user the option to click next or previous to cycle through the 5 videos and with what body xone is picked it will add that to the end of the randomization que for the rest of the week 
+// an array of the 5 body zones should be made with also 2 rest days 
+//  arms legs chest back cardio and 2 rest days 
+// utilize local storage to rember what was randomly picked the day before so that zone will not be targeted 2 days in a row 
+// 
